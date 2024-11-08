f431792fc24e3f467d170fc80da7bc44d3cf15f7
yingeli <yingeli@hotmail.com>
2024-11-08 08:57:02 +0000

    dev

--- tests/test_faster_whisper.py
+++ tests/test_faster_whisper.py
@@ -1,33 +1,36 @@
-from faster_whisper import WhisperModel
+from faster_whisper import WhisperModel, BatchedInferencePipeline
 import time
 
-model_size = "large-v3-turbo"
+model_size = "turbo"
 # Run on GPU with FP16
-#model = WhisperModel(model_size, device="cuda", compute_type="float16")
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
-model = WhisperModel(model_size, device="cpu")
+#model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 # model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
+batched_model = BatchedInferencePipeline(model=model)
+
 start = time.time()
-segments, info = model.transcribe("./audio/en-us.wav", 
-    #beam_size=5,
-    #temperature=0,  
-    #initial_prompt="Hi,", 
+#segments, info = batched_model.transcribe("./audio/en-10mins.wav", temperature=0, batch_size=16)
+segments, info = model.transcribe("./audio/en-10mins.wav", temperature=0)
+#    beam_size=1,
+#    temperature=0,  
+#    initial_prompt="Hi,", 
     #word_timestamps=True,
     #condition_on_previous_text=False, 
     #vad_filter=True,
-    )
+#    )
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
 for segment in segments:
     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
     #for word in segment.words:
-        #print("[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word))
+    #    print("[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word))
 
 duration = time.time() - start
 print(f"Duration: {duration}")
